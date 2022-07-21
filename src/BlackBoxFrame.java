92de69a2de8025531c420ede33ba826849d88f1f
Harlan Howe <hhowe7@gatech.edu>
2022-07-21 12:11:02 -0500

    Added button panel with reveal button.

--- src/BlackBoxFrame.java
+++ src/BlackBoxFrame.java
@@ -1,22 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BlackBoxFrame extends JFrame
+public class BlackBoxFrame extends JFrame implements ActionListener
 {
     private BlackBoxPanel myPanel;
+    private JButton revealButton, resetButton;
 
     public BlackBoxFrame()
     {
         super("Black Box");
-        setSize(800,800);
+        setSize(800, 800);
         setResizable(false);
         getContentPane().setLayout(new BorderLayout());
         myPanel = new BlackBoxPanel();
-        getContentPane().add(myPanel,BorderLayout.CENTER);
+        getContentPane().add(myPanel, BorderLayout.CENTER);
+        getContentPane().add(createButtonPanel(), BorderLayout.SOUTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
     }
 
+    public JPanel createButtonPanel()
+    {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
 
+        revealButton = new JButton("Reveal");
+        revealButton.addActionListener(this);
+        panel.add(revealButton);
+
+        return panel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource() == revealButton)
+        {
+            myPanel.revealAllBalls();
+        }
+    }
 }
